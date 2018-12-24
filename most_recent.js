4b475f5dbb1be274ce32219358bbddd66b79aace
John Comeau <jc@unternet.net>
2018-12-24 08:20:22 -0800

    corrected comment

--- most_recent.js
+++ most_recent.js
@@ -1,5 +1,5 @@
 var mostRecent = "https://www.facebook.com/?sk=h_chr";
 var selector = 'a[href="https://www.facebook.com/?ref=logo"]';
 var nodes = document.querySelectorAll(selector);
-// foreach construct doesn't work for some reason I don't understand
+// for...in construct doesn't work: https://stackoverflow.com/a/53915628/493161
 for (var i = 0; i < nodes.length; i++) nodes[i].href = mostRecent;
