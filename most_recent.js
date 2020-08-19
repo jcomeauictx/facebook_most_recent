95c0e69d7e131849118b6a2278433e654f14d363
John Comeau <jc@unternet.net>
2020-08-19 16:57:24 -0700

    The "new" [f] logo is being matched and replaced, but "?sk=h_chr" is lost

--- most_recent.js
+++ most_recent.js
@@ -1,5 +1,10 @@
-var mostRecent = "https://www.facebook.com/?sk=h_chr";
-var selector = 'a[href="https://www.facebook.com/?ref=logo"]';
+var mostRecent = "https://facebook.com/?sk=h_chr";
+var selector = [
+	'a[href="/"]',
+	'a[href="https://www.facebook.com"]',
+	'a[href="https://www.facebook.com/"]',
+	'a[href="https://www.facebook.com/?ref=logo"]'
+].join(",");
 var nodes = document.querySelectorAll(selector);
 // for...in construct doesn't work: https://stackoverflow.com/a/53915628/493161
 for (var i = 0; i < nodes.length; i++) nodes[i].href = mostRecent;
