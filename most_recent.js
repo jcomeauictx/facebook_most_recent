fc1ddda20ae10965a337d0a6ecf2bab677f87c3d
John Comeau <jc@unternet.net>
2020-08-22 14:24:46 -0700

    finally works on new Facebook

--- most_recent.js
+++ most_recent.js
@@ -1,15 +1,7 @@
-var mostRecent = "/?sk=h_chr";
-var selector = [
-	'a[href="/"]',
-	'a[href="https://www.facebook.com"]',
-	'a[href="https://www.facebook.com/"]',
-	'a[href="https://www.facebook.com/?ref=logo"]'
-].join(",");
-var node = document.querySelector(selector);  // just get first one
-var image = node.querySelector("svg,img");
-var cleanLink = document.createElement("a");
-cleanLink.setAttribute("href", mostRecent);
-//cleanLink.appendChild(document.createTextNode("f"));
-cleanLink.appendChild(image);
-cleanLink.style.border = "1px solid red";
-node.parentNode.appendChild(cleanLink);
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript(
+    null, 
+    {code: "window.location.href = 'https://facebook.com/?sk=h_chr'"},
+    null
+  );
+});
